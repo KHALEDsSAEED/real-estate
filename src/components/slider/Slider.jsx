fe660fe77d5563748f2ebe5ad3813492386513aa
KhaledSaeed18 <khaled18saeed@gmail.com>
2024-07-06 03:19:43 +0300

    image slider from scratch

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { useState } from "react";
+import "./slider.scss";
 
-function Slider() {
+function Slider({ images }) {
+    const [imageIndex, setImageIndex] = useState(null);
+
+    const changeSlide = (direction) => {
+        if (direction === "left") {
+            if (imageIndex === 0) {
+                setImageIndex(images.length - 1);
+            } else {
+                setImageIndex(imageIndex - 1);
+            }
+        } else {
+            if (imageIndex === images.length - 1) {
+                setImageIndex(0);
+            } else {
+                setImageIndex(imageIndex + 1);
+            }
+        }
+    };
+
     return (
-        <div>Slider</div>
-    )
+        <div className="slider">
+            {imageIndex !== null && (
+                <div className="full-slider">
+                    <div className="arrow" onClick={() => changeSlide("left")}>
+                        <img src="/arrow.png" alt="img" />
+                    </div>
+                    <div className="img-container">
+                        <img src={images[imageIndex]} alt="img" />
+                    </div>
+                    <div className="arrow" onClick={() => changeSlide("right")}>
+                        <img src="/arrow.png" className="right" alt="img" />
+                    </div>
+                    <div className="close" onClick={() => setImageIndex(null)}>
+                        X
+                    </div>
+                </div>
+            )}
+            <div className="big-image">
+                <img src={images[0]} alt="img" onClick={() => setImageIndex(0)} />
+            </div>
+            <div className="small-images">
+                {images.slice(1).map((image, index) => (
+                    <img
+                        src={image}
+                        alt="img"
+                        key={index}
+                        onClick={() => setImageIndex(index + 1)}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default Slider
+export default Slider;
