f0c8234afa3eb7cd7f33b3a8bf1171fb1123cb8e
KhaledSaeed18 <khaled18saeed@gmail.com>
2024-08-31 13:38:21 +0300

    list and single page

--- src/pages/listPage/listPage.jsx
+++ src/pages/listPage/listPage.jsx
@@ -8,22 +8,19 @@ function listPage() {
 
     const data = listData;
 
-    return (
-
-        <div className="list-page">
-            <div className="list-container">
-                <div className="wrapper">
-                    <Filter />
-                    {data.map(item => (
-                        <Card key={item.id} item={item} />
-                    ))}
-                </div>
-            </div>
-            <div className="map-container">
-                <Map items={data} />
+    return <div className="listPage">
+        <div className="listContainer">
+            <div className="wrapper">
+                <Filter />
+                {data.map(item => (
+                    <Card key={item.id} item={item} />
+                ))}
             </div>
         </div>
-    )
+        <div className="mapContainer">
+            <Map items={data} />
+        </div>
+    </div>;
 
 }
 
